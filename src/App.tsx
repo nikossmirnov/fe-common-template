cd3668a410a09e99c7d8aabde97fb550ff621301
nsmirnov <nsmirnov@accesssoftek.com>
2023-11-22 16:27:23 +0700

    reseting styles, adding themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './index.scss';
 import AboutPageAsync from './pages/AboutPage/AboutPage.async';
 import MainPageAsync from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+
+export enum Theme {
+    Dark = 'dark',
+    Light = 'light',
+}
 
 export const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.Dark);
+
+    const handleThemeChange = () => {
+        setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
+    };
     return (
-        <div >
+        <div className={`app ${theme}`}>
+            <button onClick={handleThemeChange}>Change theme</button>
             <Link to={"/"}>Main</Link>
             <Link to={"/about"}>About</Link>
                 <Suspense fallback={<div>Loading...</div>} >
